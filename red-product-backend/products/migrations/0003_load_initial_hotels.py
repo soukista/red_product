725f08a4fdf4eb27eb87c13345e927048e58e023
soukista <kanesoukeye538@gmail.com>
2026-07-19 20:15:24 +0000

    feat: auto-create default Admin user before loading hotels fixture

--- red-product-backend/products/migrations/0003_load_initial_hotels.py
+++ red-product-backend/products/migrations/0003_load_initial_hotels.py
@@ -2,6 +2,18 @@ from django.db import migrations
 from django.core.management import call_command
 
 def load_fixtures(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    # Créer l'administrateur par défaut (ID=1) s'il n'existe pas
+    if not User.objects.filter(id=1).exists():
+        User.objects.create_user(
+            username='admin@redproduct.com',
+            email='admin@redproduct.com',
+            password='AdminPassword123!',
+            first_name='Admin RED',
+            is_staff=True,
+            is_superuser=True
+        )
+    
     try:
         call_command('loaddata', 'products/fixtures/hotels.json')
     except Exception as e:
