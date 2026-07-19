a2e109efd10376e691ab2642d286252b889348ff
soukista <kanesoukeye538@gmail.com>
2026-07-19 20:31:10 +0000

    fix: add CORS regex pattern for all Vercel domains

--- red-product-backend/red_backend/settings.py
+++ red-product-backend/red_backend/settings.py
@@ -132,6 +132,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
